ec9ec9692aafe57ee7ce82c52c5001d6f56ed447
Riya Gajjar <120581578+Riya3021@users.noreply.github.com>
2023-06-01 12:49:20 +0530

    Add files via upload

---
+++ Task/Day 10/src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+import CartItem from "./pages/CartItem";
+import UpdateProfile from "./pages/UpdateProfile";
+//import { AuthWrapper } from "./context/auth";
+
+function App() {
+  // const isLoggedIn = window.localStorage.getItem("loggedIn");
+  return (
+    <>
+      <BrowserRouter>
+      {/* <AuthWrapper> */}
+        <Routes>
+          <Route path="/" Component={Register} />
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <Route
+            path="/productlist"
+            Component={window.localStorage.getItem("loggedIn",true) ? ProductList : Login}
+          />
+          <Route
+            path="/cartitem"
+            Component={window.localStorage.getItem("loggedIn",true) ? CartItem : Login}
+          />
+          <Route
+            path="/updateprofile"
+            Component={window.localStorage.getItem("loggedIn",true) ? UpdateProfile : Login}
+          />
+        </Routes>
+        {/* </AuthWrapper> */}
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
